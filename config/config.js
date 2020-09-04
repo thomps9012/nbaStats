b12963986344165fbc7c948ac6d1ed4022237b4f
Samuel Thompson <thomps9012@gmail.com>
2020-09-04 17:41:53 -0400

    updated config for server

--- config/config.js
+++ config/config.js
@@ -2,13 +2,19 @@
 
 module.exports = {
     "development": {
+        "username": process.env.MYSQL_USER,
+        "password": process.env.MYSQL_KEY,
+        "database": process.env.MYSQL_DBNAME,
+        "host": process.env.MYSQL_HOST,
+        "dialect": "mysql",
+      },
+      "test": {
         "username": "root",
         "password": "root",
         "database": "players",
         "host": "127.0.0.1",
-        "port": 3306,
-        "dialect": "mysql"
-    },
+        "dialect": "mysql",
+      },
     "production": {
         "use_env_variable": "JAWSDB_URL",
         "dialect": "mysql"
