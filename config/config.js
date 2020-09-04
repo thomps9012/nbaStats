01f9f0f728371247953b8c82c879d74405680580
Samuel Thompson <thomps9012@gmail.com>
2020-09-04 18:02:37 -0400

    reworded config script

--- config/config.js
+++ config/config.js
@@ -2,10 +2,10 @@
 
 module.exports = {
     "development": {
-        "username": process.env.MYSQL_USER,
-        "password": process.env.MYSQL_KEY,
-        "database": process.env.MYSQL_DBNAME,
-        "host": process.env.MYSQL_HOST,
+        "username": "root",
+        "password": "root",
+        "database": "players",
+        "host": "127.0.0.1",
         "dialect": "mysql",
       },
       "test": {
